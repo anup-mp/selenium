839fd295e52427c0fec16d8d7c534672af3860b0
unknown <sontrng@BG1WS0394.SONATA.LOCAL>
2019-03-19 16:25:31 +0530

    first committtt

---
+++ src/com/sonata/CustTest.java
@@ -0,0 +1,29 @@
+package com.sonata;
+
+
+	//package com.sonata.custTest;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	public class CustTest {
+
+	public static void main(String[] args) throws InterruptedException {
+
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://localhost:8083/CustomerRegistration1/CustomerDetails.html");
+	 
+	Thread.sleep(3000);
+	 
+	driver.findElement(By.name("t1")).sendKeys("Piyush Kataria");
+	driver.findElement(By.name("t2")).sendKeys("piyush34@gmail.com");
+	driver.findElement(By.name("t3")).sendKeys("987456321");
+	 
+	driver.findElement(By.xpath("//input[@type='submit']")).click();
+	 
+	}
+
+	}
+
